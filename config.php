cb3f1c6be1b53e991c6d546bf286df95c663b617
johnpaulmaja <60122972+johnpaulmaja@users.noreply.github.com>
2020-02-06 19:00:21 +0800

    Update config.php

--- config.php
+++ config.php
@@ -3,5 +3,5 @@ session_start();
 define('DB_SERVER','dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com');
 define('DB_USERNAME','admin');
 define('DB_PASSWORD','root1234');
-define('DB_DATABASE','db_1820677');
+define('DB_DATABASE','db_1820681');
 ?>
