505a27e49227c07d033a18e054c24dbd13ef15e7
johnpaulmaja <60122972+johnpaulmaja@users.noreply.github.com>
2020-02-06 19:13:18 +0800

    Update json-1.php

--- json-1.php
+++ json-1.php
@@ -1,10 +1,10 @@
 <?php
 include 'config.php';
-include 'class.Monsters.php';
+include 'class.Books.php';
 header('Content-Type: application/json');
-$monsters = new Monsters();
-$list=$monsters->get_Monsters();
-echo "{\"Monsters\":";
+$books = new Books();
+$list=$books->get_Books();
+echo "{\"Books\":";
 echo json_encode($list);
 echo "}";
 ?>
