f2886ddc4eec554706d7d1ad85be4dbfd542142a
johnpaulmaja <60122972+johnpaulmaja@users.noreply.github.com>
2020-02-06 19:31:08 +0800

    Update json-1.php

--- json-1.php
+++ json-1.php
@@ -1,9 +1,12 @@
 <?php
 include 'config.php';
 include 'class.Books.php';
+
 header('Content-Type: application/json');
+
 $books = new Books();
 $list=$books->get_Books();
+
 echo "{\"Books\":";
 echo json_encode($list);
 echo "}";
