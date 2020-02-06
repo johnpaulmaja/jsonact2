862868cb7b66d3c1dbcb732c5aad830bc382d777
johnpaulmaja <60122972+johnpaulmaja@users.noreply.github.com>
2020-02-06 19:10:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,27 @@
 <?php
-$json = file_get_contents("https://json-activity-vaflor.herokuapp.com/json-1.php");
+$json = file_get_contents("https://maja-act-2.herokuapp.com/json-1.php");
 
 $data = json_decode($json,true);
-$list = $data['Monsters'];
+$list = $data['Books'];
 
 
 ?>
 
-<h1>MONSTERS</h1>
+<h1>BOOKS</h1>
 
 <table border="1px">
     <tr class="head">
-        <td>Monster</td>
-        <td>Classification</td>
-        <td>Immunity</td>
-        <td>Tactics</td>
-        <td>Occurence</td>
+        <td>Title</td>
+        <td>Author</td>
+        <td>Description</td>
     </tr>
 <?php
 foreach($list as $value){
     ?>
     <tr>
-        <td><?php echo $value['Monster'];?></td>
-        <td><?php echo $value['Classification'];?></td>
-        <td><?php echo $value['Immunity'];?></td>
-        <td><?php echo $value['Tactics'];?></td>
-        <td><?php echo $value['Occurence'];?></td>
+        <td><?php echo $value['Title'];?></td>
+        <td><?php echo $value['Author'];?></td>
+        <td><?php echo $value['Description'];?></td>
     </tr>
 <?php
 }
